083b48faf4cbb8d5056a97be67d70b582d9b274e
sataratimoori <sataratimoori@gmail.com>
2026-07-07 04:42:39 +0430

    add best tours section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+const heroSwiper = new Swiper('.hero-swiper', {
   slidesPerView: 2,
   spaceBetween: 10,
   slidesPerGroupSkip: 1,
@@ -13,4 +13,25 @@ const swiper = new Swiper('.swiper', {
   autoplay: {
     pauseOnMouseEnter: true
   }
+});
+
+const panoramaSwiper = new Swiper(".panorama-swiper", {
+    effect: "coverflow",
+    loop: true,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 3,
+
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+
+    coverflowEffect: {
+        rotate: 40,
+        stretch: 0,
+        depth: 200,
+        modifier: 1,
+        slideShadows: true,
+    },
 });
